4a40328fabdc67234f651ed28e01fe19635544b5
hung nguyen <hungnguyenisme84@gmail.com>
2023-05-19 10:16:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab3_ph40917/Bai2.java
@@ -0,0 +1,14 @@
+package com.example.lab3_ph40917;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai2);
+    }
+}
